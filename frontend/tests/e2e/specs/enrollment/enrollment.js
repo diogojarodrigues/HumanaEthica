af97a44d7d7baa20fa79e83757482c648f2df86b
Pedro Ribeiro <pedro.melo.ribeiro@tecnico.ulisboa.pt>
2024-03-24 20:52:46 +0000

    test: Test List Enrollment - As member, check if the first activity has 0 applications
    Refs: #219, #220

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -10,7 +10,8 @@ describe('Enrollment', () => {
 
     it('create enrollment', () => {
 
-        const NAME = 'MOTIVATION';
+        const MOTIVATION = 'MOTIVATION';
+        const NUMBER = '0';
 
         cy.intercept('GET', '/users/*/getInstitution').as('getInstitution');
         cy.intercept('GET', '/activities').as('getActivities');
@@ -26,6 +27,13 @@ describe('Enrollment', () => {
         // Check if there are 3 activities
         cy.get('[data-cy="memberActivitiesTable"] tbody tr').should('have.length', 3);
 
+        // Check if the first activity has 0 enrollments
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+            .should('have.length', 3)
+            .eq(0)
+            .children()
+            .eq(4)
+            .should('contain', NUMBER)
 
         // Login as volunteer
         cy.logout();
@@ -42,7 +50,7 @@ describe('Enrollment', () => {
         });
 
         // Fill the form
-        cy.get('[data-cy="motivationInput"]').type(NAME);
+        cy.get('[data-cy="motivationInput"]').type(MOTIVATION);
         cy.get('[data-cy="saveEnrollment"]').click()
         cy.wait('@createEnrollment')
         
@@ -64,7 +72,7 @@ describe('Enrollment', () => {
         //Check if the first activity has one enrollment
         cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').should('have.length', 1);
         //Check if the enrollment has motivation
-        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').first().should('contain', NAME);
+        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').first().should('contain', MOTIVATION);
         cy.logout();
 
     });
