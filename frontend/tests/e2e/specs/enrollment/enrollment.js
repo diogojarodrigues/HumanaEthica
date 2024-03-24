43533c400897eb06a6f911948a403a58a0d52b84
Pedro Ribeiro <pedro.melo.ribeiro@tecnico.ulisboa.pt>
2024-03-24 21:12:07 +0000

    test: Test List Enrollment - As member, check if the first activity has 1 application
    Refs: #219, #221

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -12,11 +12,13 @@ describe('Enrollment', () => {
 
         const MOTIVATION = 'MOTIVATION';
         const NUMBER = '0';
+        const NEWNUMBER = '1';
 
         cy.intercept('GET', '/users/*/getInstitution').as('getInstitution');
         cy.intercept('GET', '/activities').as('getActivities');
         cy.intercept('POST', '/activities/*/enrollments').as('createEnrollment');    
 
+        // Login as member
         cy.demoMemberLogin()
 
         // Get the activities
@@ -24,25 +26,23 @@ describe('Enrollment', () => {
         cy.get('[data-cy="activities"]').click();
         cy.wait('@getInstitution');
 
-        // Check if there are 3 activities
-        cy.get('[data-cy="memberActivitiesTable"] tbody tr').should('have.length', 3);
-
-        // Check if the first activity has 0 enrollments
+        // Check if there are 3 activities && the first activity has 0 Applications
         cy.get('[data-cy="memberActivitiesTable"] tbody tr')
             .should('have.length', 3)
             .eq(0)
             .children()
             .eq(4)
-            .should('contain', NUMBER)
+            .should('contain', NUMBER);
+        
+        cy.logout();
+
 
         // Login as volunteer
-        cy.logout();
         cy.demoVolunteerLogin();
         
         // Go to volunteer activities view
         cy.get('[data-cy="volunteerActivities"]').click();
         cy.wait('@getActivities');
-
         
         // Create enrollment
         cy.get('[data-cy="volunteerActivitiesTable"]').within(() => {
@@ -60,20 +60,32 @@ describe('Enrollment', () => {
         //Login as member
         cy.demoMemberLogin();
         cy.intercept('GET', '/activities/*/enrollments').as('showEnrollments');
+        
         //Get the activities
         cy.get('[data-cy="institution"]').click();
         cy.get('[data-cy="activities"]').click();
         cy.wait('@getInstitution');
+
+        //Check if there are 3 activities && the first activity has 1 Applications
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .should('have.length', 3)
+        .eq(0)
+        .children()
+        .eq(4)
+        .should('contain', NEWNUMBER);
+
         //Get the enrollments for the first activity
         cy.get('[data-cy="memberActivitiesTable"] tbody tr').first().within(() => {
             cy.get('[data-cy="showEnrollments"]').click();
         })
         cy.wait('@showEnrollments');
-        //Check if the first activity has one enrollment
-        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').should('have.length', 1);
-        //Check if the enrollment has motivation
-        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').first().should('contain', MOTIVATION);
-        cy.logout();
+        
+        //Check if the first activity has one enrollment && the enrollment has motivation
+        cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+            .should('have.length', 1)
+            .eq(0)
+            .should('contain', MOTIVATION);
 
+        cy.logout();
     });
 });
